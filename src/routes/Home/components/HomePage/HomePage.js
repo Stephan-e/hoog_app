d781485fc9ca6921f12b272aa59012d58c8ece0a
TarCode <tarcode33@gmail.com>
2018-11-28 12:48:24 +0200

    updated home css

--- src/routes/Home/components/HomePage/HomePage.js
+++ src/routes/Home/components/HomePage/HomePage.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { paths } from 'constants'
-import classes from './HomePage.scss'
-import logo from 'logo.svg';
+// import classes from './HomePage.scss'
+import logo from './logo.svg';
 const authWrapperUrl = 'https://github.com/mjrussell/redux-auth-wrapper'
 const reactRouterUrl = 'https://github.com/ReactTraining/react-router'
 
 export const Home = () => (
-  <div className={classes.container}>
-    <div className="flex-row-center">
-      <h2>The self-growing plant box!</h2>
-    </div>
-    <div className="flex-row-center">
-    <img src={logo} />
+  <div style={{
+    height: '90vh',
+  }}>
+    <div style={{
+      position: 'absolute',
+      top: '10vh',
+      left: 0,
+      width: '100%'
+    }}>
+      <h2 style={{
+        color: '#fff',
+        fontSize: '30px',
+        zIndex: 3,
+        paddingTop: '30vh'
+      }}>The self-growing plant box!</h2>
+      <img  style={{
+        position: 'absolute',
+        width: '80%',
+        right: 0, 
+        top: 0,
+        zIndex: 2
+      }} src={logo} />
     </div>
   </div>
 )
